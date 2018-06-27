85bd201a4958425e53c27f5cd816d3b460844a81
ANA LOPEZ MANCISIDOR RUEDA <ana.lopez@es.ibm.com>
2018-06-27 11:19:23 +0000

    change comment by Ana 2

--- chaincode/src/marbles/marbles.go
+++ chaincode/src/marbles/marbles.go
@@ -1,5 +1,5 @@
 /*
-Comment Ana
+Comment Ana 2
 Licensed to the Apache Software Foundation (ASF) under one
 or more contributor license agreements.  See the NOTICE file
 distributed with this work for additional information
