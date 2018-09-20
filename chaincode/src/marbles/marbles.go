a51ab5c96150e5ee1b84bad85f9cdbea9358fca2
ANA LOPEZ MANCISIDOR RUEDA <ana.lopez@es.ibm.com>
2018-09-20 14:46:55 +0000

    cambio demo

--- chaincode/src/marbles/marbles.go
+++ chaincode/src/marbles/marbles.go
@@ -1,5 +1,5 @@
 /*
-Ana comment change
+Ana comment change. ADEMO 
 Licensed to the Apache Software Foundation (ASF) under one
 or more contributor license agreements.  See the NOTICE file
 distributed with this work for additional information
